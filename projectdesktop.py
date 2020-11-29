8b74edcd70871ba6e2edc392e8afef570cb9777c
siluthfi <luthfialredia123@gmail.com>
2020-11-29 21:44:24 +0700

    Create projectdesktop.py

---
+++ projectdesktop.py
@@ -0,0 +1,10 @@
+import requests
+import time
+from plyer import notification
+
+notification.notify(
+	title = "Belajar membuat notif menggunakan python",
+	message = 'anjing lu',
+	app_icon = "Paomedia-Small-N-Flat-Bell.ico",
+	timeout = 10
+	)
